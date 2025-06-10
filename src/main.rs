34af32cf8dd09f63f8960615d6055d934e474d5b
Shashank <shashank.verma2002@gmail.com>
2025-06-10 13:05:33 +0530

    2_2: file write and progress indicator
    Signed-off-by: Shashank <shashank.verma2002@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::{fs, io::Write};
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
 
-    println!("P3\n{image_width} {image_height}\n255");
+    // image file
+    let mut ppm_file =
+        fs::File::create("image.ppm").expect("Failed to create/truncate ppm image file");
+
+    // write ppm header to image file
+    ppm_file
+        .write_fmt(format_args!("P3\n{image_width} {image_height}\n255\n"))
+        .expect("Failed to write header");
 
     for j in 0..image_height {
+        print!("\rScanlines remaining: {} ", image_height - j);
+        std::io::stdout().flush().unwrap();
+
         for i in 0..image_width {
             let r = i as f32 / (image_width - 1) as f32;
             let g = j as f32 / (image_height - 1) as f32;
@@ -13,7 +25,12 @@ fn main() {
             let ir = (255.999 * r) as i32;
             let ig = (255.999 * g) as i32;
             let ib = (255.999 * b) as i32;
-            println!("{ir} {ig} {ib}");
+
+            // write pixel to image file
+            ppm_file
+                .write_fmt(format_args!("{ir} {ig} {ib}\n"))
+                .expect("Failed to write pixel");
         }
     }
+    println!("\rDone                  ");
 }
