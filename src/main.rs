340fc49a09dd3136e21dc2149d8c9f01cdf6e3e2
Shashank <shashank.verma2002@gmail.com>
2025-06-10 12:58:07 +0530

    2_1: ppm image
    Signed-off-by: Shashank <shashank.verma2002@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello world !");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{image_width} {image_height}\n255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b: f32 = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
