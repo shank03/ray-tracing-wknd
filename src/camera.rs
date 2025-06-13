bf47d3b282b7400121fce067ae3dc13890611df7
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-13 18:57:02 +0530

    7: working camera
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/camera.rs
+++ src/camera.rs
@@ -1,15 +1,92 @@
-use std::f64::INFINITY;
+use std::{f64::INFINITY, fs, io::Write};
 
 use crate::{
-    color::Color,
+    color::{self, Color},
     hittable::{HitRecord, Hittable},
     ray::Ray,
-    vec3::{SliceOp, SliceStruct},
+    vec3::{self, Point3, SliceOp, SliceStruct, Vec3},
 };
 
-pub struct Camera {}
+pub struct Camera {
+    image_width: i32,
+    image_height: i32,
+    center: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
 
 impl Camera {
+    pub fn new(aspect_ratio: f64, image_width: i32) -> Self {
+        // calculate image height, it should be at least 1
+        let image_height = (image_width as f64 / aspect_ratio) as i32;
+        let image_height = image_height.max(1);
+
+        // camera
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+        let center = vec3::init();
+
+        // calculate the vectors across the horizontal and down the vertical viewport edges
+        let viewport_u = [viewport_width, 0.0, 0.0];
+        let viewport_v = [0.0, -viewport_height, 0.0];
+
+        // calculate the horizontal and vertical delta vectors from pixel to pixel
+        let pixel_delta_u = viewport_u.div_f(image_width as f64);
+        let pixel_delta_v = viewport_v.div_f(image_height as f64);
+
+        // calculate the location of the upper left pixel
+        let viewport_upper_left = center
+            .sub([0.0, 0.0, focal_length])
+            .sub(viewport_u.div_f(2.0))
+            .sub(viewport_v.div_f(2.0));
+        let pixel00_loc = viewport_upper_left.add(pixel_delta_u.add(pixel_delta_v).mul_f(0.5));
+
+        Self {
+            image_width,
+            image_height,
+            center,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
+    }
+
+    pub fn render(&self, world: &dyn Hittable) {
+        // image file
+        let mut ppm_file =
+            fs::File::create("image.ppm").expect("Failed to create/truncate ppm image file");
+
+        // write ppm header to image file
+        ppm_file
+            .write_fmt(format_args!(
+                "P3\n{} {}\n255\n",
+                self.image_width, self.image_height
+            ))
+            .expect("Failed to write header");
+
+        for j in 0..self.image_height {
+            print!("\rScanlines remaining: {} ", self.image_height - j);
+            std::io::stdout().flush().unwrap();
+
+            for i in 0..self.image_width {
+                let pixel_center = self
+                    .pixel00_loc
+                    .add(self.pixel_delta_u.mul_f(i as f64))
+                    .add(self.pixel_delta_v.mul_f(j as f64));
+                let ray_direction = pixel_center.sub(self.center);
+                let r = Ray::new(self.center, ray_direction);
+
+                let pixel_color = Self::ray_color(r, world);
+
+                // write pixel to image file
+                color::write_color(&mut ppm_file, pixel_color);
+            }
+        }
+        println!("\rDone                  ");
+    }
+
     fn ray_color(r: Ray, world: &dyn Hittable) -> Color {
         let mut record = HitRecord::init();
         if world.hit(&r, 0.0..INFINITY, &mut record) {
