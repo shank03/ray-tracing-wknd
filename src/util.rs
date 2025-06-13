149d00bb5a45ae3e3b7572d58a5c42e07769f981
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-13 23:20:56 +0530

    8_1: util: add random float generator
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,16 @@
 use std::f64::consts::PI;
 
+use rand::Rng;
+
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
+}
+
+pub fn random_float() -> f64 {
+    let mut rng = rand::rng();
+    rng.random_range(0.0..1.0)
+}
+
+pub fn random_min_max(min: f64, max: f64) -> f64 {
+    min + (max - min) * random_float()
 }
