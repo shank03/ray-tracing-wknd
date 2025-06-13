89de59813d8ceb395f44ceaca55dc2443ce4b0e7
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-13 14:52:11 +0530

    6_4: hittable: add front-facing in record
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/hittable.rs
+++ src/hittable.rs
@@ -2,13 +2,14 @@ use std::ops::Range;
 
 use crate::{
     ray::Ray,
-    vec3::{self, Point3, Vec3},
+    vec3::{self, Point3, SliceOp, Vec3},
 };
 
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
 }
 
 impl HitRecord {
@@ -17,6 +18,18 @@ impl HitRecord {
             p: vec3::init(),
             normal: vec3::init(),
             t: 0.0,
+            front_face: false,
+        }
+    }
+
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: Vec3) {
+        self.front_face = ray.direction().dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            // ray is outside the sphere
+            outward_normal
+        } else {
+            // ray is inside the sphere
+            outward_normal.neg()
         }
     }
 }
