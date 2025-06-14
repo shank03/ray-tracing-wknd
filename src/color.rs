df05288e890564e02bdb19864bea894c8064e7b7
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-14 10:32:15 +0530

    8_2: multi-sample write_color
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/color.rs
+++ src/color.rs
@@ -7,10 +7,11 @@ pub type Color = Vec3;
 pub fn write_color(file: &mut fs::File, pixel: Color) {
     let [r, g, b] = pixel;
 
+    let range = 0.000..0.999;
     let [r, g, b] = [
-        (255.999 * r) as i32,
-        (255.999 * g) as i32,
-        (255.999 * b) as i32,
+        (256.0 * r.clamp(range.start, range.end)) as u8,
+        (256.0 * g.clamp(range.start, range.end)) as u8,
+        (256.0 * b.clamp(range.start, range.end)) as u8,
     ];
 
     file.write_fmt(format_args!("{r} {g} {b}\n"))
