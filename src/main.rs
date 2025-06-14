597652aba3abc38a8a2558beb0f75aca4b8cabae
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-14 15:09:44 +0530

    10_5: scene with metal spheres
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,21 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
 
+    let material_ground = material::Lambertian::new([0.8, 0.8, 0.0]);
+    let material_center = material::Lambertian::new([0.1, 0.2, 0.5]);
+    let material_left = material::Metal::new([0.8, 0.8, 0.8]);
+    let material_right = material::Metal::new([0.8, 0.6, 0.2]);
+
     // world
     let mut world = hittable::HittableList::new();
-    world.push(sphere::Sphere::new([0.0, 0.0, -1.0], 0.5));
-    world.push(sphere::Sphere::new([0.0, -100.5, -1.0], 100.0));
+    world.push(sphere::Sphere::new(
+        [0.0, -100.5, -1.0],
+        100.0,
+        &material_ground,
+    ));
+    world.push(sphere::Sphere::new([0.0, 0.0, -1.2], 0.5, &material_center));
+    world.push(sphere::Sphere::new([-1.0, 0.0, -1.0], 0.5, &material_left));
+    world.push(sphere::Sphere::new([1.0, 0.0, -1.0], 0.5, &material_right));
 
     let cam = camera::Camera::new(aspect_ratio, image_width, 100, 50);
     cam.render(&world);
