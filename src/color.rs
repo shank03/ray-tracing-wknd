48cd4d853fb017f0b473973918ec99ee6dc7b8d2
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-15 14:24:10 +0530

    9_5: color: linear to gamma
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/color.rs
+++ src/color.rs
@@ -4,8 +4,13 @@ use crate::vec3::Vec3;
 
 pub type Color = Vec3;
 
+fn linear_to_gamma(linear: f64) -> f64 {
+    if linear > 0.0 { linear.sqrt() } else { 0.0 }
+}
+
 pub fn write_color(file: &mut fs::File, pixel: Color) {
     let [r, g, b] = pixel;
+    let [r, g, b] = [linear_to_gamma(r), linear_to_gamma(g), linear_to_gamma(b)];
 
     let range = 0.000..0.999;
     let [r, g, b] = [
